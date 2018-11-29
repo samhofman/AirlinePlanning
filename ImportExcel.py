92606d159d734da4f81e6c8a07aac9b4d1855445
wouter-duchateau <wouter.duchateau19@gmail.com>
2018-11-29 13:53:41 +0100

    Excel import

--- ImportExcel.py
+++ ImportExcel.py
@@ -4,15 +4,31 @@ Spyder Editor
 
 This is a temporary script file.
 """
+import numpy as np
+import xlrd as xl
 
-# Import pandas
-import pandas as pd
+book = xl.open_workbook('Datasheet.xlsx')
 
-# Assign spreadsheet filename to 'data'
-data = 'Datasheet.xlsx'
 
-# Load spreadsheet
-xl = pd.ExcelFile(data)
+def get_cell_range(sheet, start_col, start_row, end_col, end_row):
+    return [book.sheet_by_index(sheet-1).row_values(row, start_colx=start_col, end_colx=end_col+1) for row in xrange(start_row, end_row+1)]
+
+
+airport_data = get_cell_range(2, 2, 6, 26, 9)   # C7 to Z10
+
+demand_pw = get_cell_range(2, 2, 15, 21, 34)    # C16 to V35
+
+demand_pw_hs = get_cell_range(2, 2, 37, 26, 60) # C38 to Z61
+
+demand_ow_ls = get_cell_range(2, 2, 63, 26, 86) # C64 to Z87
+
+competition = get_cell_range(2, 2, 89, 26, 112) # C90 to Z113 
+
+
+
+
+
+
 
 
 
