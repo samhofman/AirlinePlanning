5e79867c0a6b545c6b617343f04b1f90a4ec7fed
wouter-duchateau <wouter.duchateau19@gmail.com>
2018-11-29 11:10:36 +0100

    excel file geupload

---
+++ ImportExcel.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+# Import pandas
+import pandas as pd
+
+# Assign spreadsheet filename to 'data'
+data = 'Datasheet.xlsx'
+
+# Load spreadsheet
+xl = pd.ExcelFile(data)
+
+
+
