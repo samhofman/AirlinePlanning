2c97f3488eba9d0166b621d60686e8aaebe061d9
Sam Hofman <hofmansam@hotmail.com>
2018-12-24 16:32:16 +0100

    P2 done

--- OF_P2.py
+++ OF_P2.py
@@ -28,6 +28,7 @@ hflow    = {}    #w_i,j
 flights  = {}    #z_i,j^k
 aircraft = {}    #AC^k
 aircraft_leased = {}    #m^k
+aircraft_sold = {}
 
 
 ### CREATE DECISION VARIABLES ###########################################################################################
@@ -45,12 +46,15 @@ for i in range(nodes):
             #            name="AC_%s"%(k))
             aircraft_leased[k] = m.addVar(vtype=GRB.INTEGER, lb=0, #No. of extra leased aircraft
                         name="m_%s"%(k)) 
+            aircraft_sold[k] = m.addVar(vtype=GRB.INTEGER, lb=0, #No. of extra leased aircraft
+                        name="n_%s"%(k)) 
+            
 m.update()
 
 
 ##### OBJECTIVE FUNCTION #################################################################################################
 
-obj = grb.quicksum(grb.quicksum( y(i,j)*distance(i,j)*(hflow[i,j]+flow[i,j]) -  grb.quicksum((flights[i,j,k]*cost_fact(i,j)*(Cx_ar[0][k]+Ct[0][k]*distance(i,j)/speed[0][k]+Cf[0][k]*F22*distance(i,j)/1.5)) for k in range(commod))for j in range(nodes))for i in range(nodes)) - grb.quicksum((AC[0][k]+aircraft_leased[k])*Cl[0][k]+2000.*aircraft_leased[k] for k in range(commod))
+obj = grb.quicksum(grb.quicksum( y(i,j)*distance(i,j)*(hflow[i,j]+flow[i,j]) -  grb.quicksum((flights[i,j,k]*cost_fact(i,j)*(Cx_ar[0][k]+Ct[0][k]*distance(i,j)/speed[0][k]+Cf[0][k]*F22*distance(i,j)/1.5)) for k in range(commod))for j in range(nodes))for i in range(nodes)) - grb.quicksum((AC[0][k]+aircraft_leased[k]-aircraft_sold[k])*Cl[0][k]+2000.*aircraft_leased[k]+8000.*aircraft_sold[k] for k in range(commod))
 
 m.setObjective(obj,GRB.MAXIMIZE) #fill in obj instead of m.getObjective
 
@@ -102,7 +106,7 @@ for i in range(nodes):
         for k in range(commod):
             m.addConstr(grb.quicksum(grb.quicksum((((distance(i,j)/speed[0][k])+(TAT(j,k)/60.))*flights[i,j,k])for j in range(nodes))for i in range(nodes)),
                         GRB.LESS_EQUAL,
-                        7*BT*(AC[0][k]+aircraft_leased[k]))      
+                        7*BT*(AC[0][k]+aircraft_leased[k]-aircraft_sold[k]))      
 
 ### 6 ################################################################
 # Aircraft of type k cannot fly flight leg if its range is smaller than the distance
@@ -147,9 +151,15 @@ for i in range(1,24):
 ### 10 ################################################################
 # Only 7500 seats to/from USA available per week
 print "Constraint 10 loading"
-m.addConstr(grb.quicksum(grb.quicksum((flow[i,j] + hflow[i,j] + flow[j,i] + hflow[j,i]) for j in range(20,24)) for i in range(nodes)),
+#m.addConstr(grb.quicksum(grb.quicksum((flow[i,j] + hflow[i,j] + flow[j,i] + hflow[j,i]) for j in range(20,24)) for i in range(nodes)),
+#            GRB.LESS_EQUAL,
+#            7500.)
+#for i in range(nodes):
+#    for j in range(20,24):
+#        for k in range(commod):
+m.addConstr(grb.quicksum(grb.quicksum(grb.quicksum((flights[i,j,k]+flights[j,i,k])*seats[0][k] for j in range(20,24)) for k in range(commod)) for i in range(nodes)),
             GRB.LESS_EQUAL,
-            7500.)
+            7500.) 
 
 ### 11 ################################################################
 # No more flights than slots available
@@ -158,6 +168,14 @@ for j in range(nodes):
     m.addConstr(grb.quicksum(grb.quicksum(flights[i,j,k] for k in range(commod)) for i in range(nodes)),
                 GRB.LESS_EQUAL,
                 slots[j])
+    
+### 12 ################################################################
+# Don't sell more aircraft than available
+print "Constraint 12 loading"
+for k in range(commod):         
+    m.addConstr(aircraft_sold[k],
+                GRB.LESS_EQUAL,
+                AC[0][k])
 
 m.update()
 
@@ -194,6 +212,7 @@ var_x = []
 var_w = []
 var_z = []
 var_m = []
+var_n = []
 
 
 for var in m.getVars():
@@ -206,6 +225,9 @@ for var in m.getVars():
         var_z.append([var.VarName, var.x])
     if 'm' == str(var.VarName[0]) and var.x > 0:
         var_m.append([var.VarName, var.x]) 
+    if 'n' == str(var.VarName[0]) and var.x > 0:
+        var_n.append([var.VarName, var.x]) 
+        
 # Write to csv
 with open('outP2.csv', 'wb') as myfile:
      wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
@@ -213,6 +235,7 @@ with open('outP2.csv', 'wb') as myfile:
      wr.writerows(var_w)
      wr.writerows(var_z)
      wr.writerows(var_m)
+     wr.writerows(var_n)
 
 
 
