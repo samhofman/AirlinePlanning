f9cf0f82998ba11355c3d58a96cab2bbd793422c
wouter-duchateau <wouter.duchateau19@gmail.com>
2018-12-27 18:33:57 +0100

    KPIs corrected

--- KPI_P1.py
+++ KPI_P1.py
@@ -7,25 +7,29 @@ Created on Wed Dec 26 21:11:08 2018
 
 from OF_P1 import *
 
-KPI = np.array(['FROM', 'TO', 'Distance', 'Seats', 'Pax', 'ASK', 'RPK'])
+KPI = np.array(['FROM', 'TO', 'Distance', 'Seats', 'Pax', 'ASK', 'RPK', ' ', 'RASK', 'CASK', 'Yield'])
 KPI_values = []
 
        
 for i in range(nodes):
     for j in range(nodes):
-            total_seats = grb.LinExpr.getValue(grb.quicksum(flights[i,j,k].x * seats[0][k] for k in range(commod)))
-            ASKij = distance(i,j) * total_seats
-            RPKij = grb.LinExpr.getValue(distance(i,j) * flow[i,j])
-            if RPKij > 0.:
-                KPI_values = np.array([i,j,distance(i,j),total_seats, flow[i,j].x,ASKij,RPKij])
-                KPI = np.vstack( (KPI, KPI_values))
+        total_seats = grb.LinExpr.getValue(grb.quicksum(flights[i,j,k].x * seats[0][k] for k in range(commod)))
+        ASKij = distance(i,j) * total_seats
+        RPKij = grb.LinExpr.getValue(distance(i,j) * (flow[i,j]+hflow[i,j]))
+        revenueij  = grb.LinExpr.getValue((5.9*dist_fact(i,j)+0.043)*distance(i,j)*(hflow[i,j] + flow[i,j]))
+        costij = grb.LinExpr.getValue(grb.quicksum((flights[i,j,k]*cost_fact(i,j)*(Cx_ar[0][k]+Ct[0][k]*distance(i,j)/speed[0][k]+Cf[0][k]*F17*distance(i,j)/1.5)) for k in range(commod)) + grb.quicksum(AC[0][k]*Cl[0][k] for k in range(commod)))
+        if ASKij > 0. and RPKij > 0.:    
+            RASK = revenueij/ASKij
+            CASK  = costij/ASKij
+            yieldij =  revenueij/RPKij
+            KPI_values = np.array([i,j,distance(i,j),total_seats, flow[i,j].x,ASKij,RPKij, ' ', RASK, CASK, yieldij])
+            KPI = np.vstack( (KPI, KPI_values))
 
 with open('KPI_P1_results.csv', 'wb') as myfile:
      wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
      wr.writerows(KPI)
 
 
- 
 
 
 #var_ASK = []    
