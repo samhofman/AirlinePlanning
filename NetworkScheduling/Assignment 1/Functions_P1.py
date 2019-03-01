f226cf01ebeeba96c5c07def3787a894728dfd42
Sam Hofman <hofmansam@hotmail.com>
2019-03-01 13:13:31 +0100

    Vraag 1 OF klaar

--- NetworkScheduling/Assignment 1/Functions_P1.py
+++ NetworkScheduling/Assignment 1/Functions_P1.py
@@ -5,6 +5,7 @@ Created on Tue Feb 26 15:09:28 2019
 @author: hofma
 """
 
+import xlsxwriter
 from Data import *
 
 ### Make cost matrix ###
@@ -16,6 +17,24 @@ for i in range(len(arcs)):
         b = arcs[i,2]
         cost[a,b] = arcs[i,3]
         
+cost = np.maximum( cost, cost.transpose() ) #Make matrix symmetric to work in both directions
+
+workbook = xlsxwriter.Workbook('Cost_P1.xlsx') #Write to Excel to check
+worksheet = workbook.add_worksheet('Cost')
+
+array = cost
+
+row = 0
+
+for col, data in enumerate(array):
+    worksheet.write_column(row, col, data)
+
+workbook.close()
+
+### Determine number of airports ###
+
+airports = len(cost)
+        
 ### Make capacity matrix ###
 
 capacity = np.zeros(shape=(16,16))
@@ -24,6 +43,20 @@ for i in range(len(arcs)):
         a = arcs[i,1]
         b = arcs[i,2]
         capacity[a,b] = arcs[i,4]
+        
+capacity = np.maximum( capacity, capacity.transpose() ) #Make matrix symmetric to work in both directions
+
+workbook = xlsxwriter.Workbook('Capacity_P1.xlsx') #Write to Excel to check
+worksheet = workbook.add_worksheet('Capacity')
+
+array = capacity
+
+row = 0
+
+for col, data in enumerate(array):
+    worksheet.write_column(row, col, data)
+
+workbook.close()
 
 ### Make quantity matrix ###
 
@@ -32,7 +65,19 @@ quantity = np.zeros(shape=(16,16))
 for i in range(len(commodities)):
     a = commodities[i,1]
     b = commodities[i,2]
-    capacity[a,b] = commodities[i,3]
+    quantity[a,b] = commodities[i,3]
+    
+workbook = xlsxwriter.Workbook('Quantity_P1.xlsx') #Write to Excel to check
+worksheet = workbook.add_worksheet('Capacity')
+
+array = quantity
+
+row = 0
+
+for col, data in enumerate(array):
+    worksheet.write_column(row, col, data)
+
+workbook.close()
     
 ### Demand function ###
 
@@ -44,3 +89,16 @@ def d(i,k):
     else:
         d = 0.0
     return d
+
+### Cost function ###
+
+def c(i,j):
+    c = cost[i,j]
+    return c
+
+### Capacity function ###
+
+def u(i,j):
+    u = capacity[i,j]
+    return u
+    
--- NetworkScheduling/Assignment 1/OF_P1.py
+++ NetworkScheduling/Assignment 1/OF_P1.py
@@ -21,8 +21,8 @@ m = grb.Model('MaxProfit')
 flow     = {}    #x_i,j_k
 
 ### CREATE DECISION VARIABLES ###########################################################################################
-for i in range(len(arcs)):
-    for j in range(len(arcs)):
+for i in range(airports):
+    for j in range(airports):
         for k in range(len(commodities)):
             flow[i,j,k] = m.addVar(vtype=GRB.INTEGER, lb=0,
                         name="x_%s,%s_%s"%(i,j,k))
@@ -32,8 +32,79 @@ m.update()
 
 ##### OBJECTIVE FUNCTION #################################################################################################
 
-obj = grb.quicksum(grb.quicksum( ))
+obj = grb.quicksum(grb.quicksum(grb.quicksum((c(i,j)*flow[i,j,k]) for i in range(airports)) for j in range(airports)) for k in range(len(commodities)))
 
 m.setObjective(obj,GRB.MINIMIZE) #fill in obj instead of m.getObjective
 
 print "Objective function created."
+
+##### CONSTRAINTS ########################################################################################################
+
+### 1 ################################################################
+print "Constraint 1 loading"
+for i in range(airports):
+    for j in range(airports):
+        for k in range(len(commodities)):
+            m.addConstr((grb.quicksum( flow[i,j,k] for j in range(airports)) - grb.quicksum(flow[j,i,k] for j in range(airports))),
+                        GRB.EQUAL,
+                        d(i,k))
+            
+### 2 ################################################################
+print "Constraint 2 loading"
+for i in range(airports):
+    for j in range(airports):
+        for k in range(len(commodities)):
+            m.addConstr(    grb.quicksum((flow[i,j,k]) for k in range(len(commodities))), 
+                        GRB.LESS_EQUAL, 
+                        u(i,j))
+            
+### 3 ################################################################
+print "Constraint 3 loading"
+for i in range(airports):
+    for j in range(airports):
+        for k in range(len(commodities)):
+            m.addConstr(    flow[i,j,k], 
+                        GRB.GREATER_EQUAL, 
+                        0.0)
+            
+m.update()
+
+### RUN ###
+
+def mycallback(model, where):
+    if where == GRB.Callback.MIP:
+        objbnd = model.cbGet(GRB.Callback.MIP_OBJBND)
+        objbst = model.cbGet(GRB.Callback.MIP_OBJBST)
+        runtime = model.cbGet(GRB.Callback.RUNTIME)
+        if runtime > 60:
+            model.terminate()
+        elif abs(objbst - objbnd) <= 0.0020 * (abs(objbst)):
+            print('Stop early - 0.20% gap achieved')
+            model.terminate()
+            
+            return;
+   
+m.optimize(mycallback)
+
+                 
+for v in m.getVars():
+    if v.x > 0:
+        print (v.varName, v.x)    
+print ('Obj:', m.objVal) 
+
+if m.SolCount == 0:
+    print("Model has no solution")
+    exit(1)
+
+var_x = []    
+
+
+for var in m.getVars():
+    # Or use list comprehensions instead 
+    if 'x' == str(var.VarName[0]) and var.x > 0:
+        var_x.append([var.VarName, var.x])
+   
+# Write to csv
+with open('Out_P1.csv', 'wb') as myfile:
+     wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+     wr.writerows(var_x)
